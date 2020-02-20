23a223b4faf0ef58553903eae03e44711b1d19d0
y-kanemitsu <y-kanemitsu@topknock.co.jp>
2020-02-20 21:20:55 +0900

    [Edit]Carendarカスタマイズ修正

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -1,5 +1,13 @@
 <template>
-    <FullCalendar default-view="dayGridMonth" :plugins="calendarPlugins" />
+    <FullCalendar
+        default-view="dayGridMonth"
+        :locale="locale"
+        :header="calendarHeader"
+        :weekends="calendarWeekends"
+        :plugins="calendarPlugins"
+        :events="calendarEvents"
+        @dateClick="handleDateClick"
+    />
 </template>
 
 <script>
@@ -7,19 +15,57 @@ import FullCalendar from '@fullcalendar/vue'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import jaLocale from '@fullcalendar/core/locales/ja' // 日本語化用
 
 export default {
     components: {
-        FullCalendar
+        FullCalendar // make the <FullCalendar> tag available
     },
     data () {
         return {
-            // 使用したいプラグインを以下で指定。事前にimportでインポートする。
+            locale: jaLocale, // 日本語化
+            // カレンダーヘッダーのデザイン
+            calendarHeader: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+            },
+            calendarWeekends: true, // 土日を表示するか
+            // カレンダーで使用するプラグイン
             calendarPlugins: [ 
                 dayGridPlugin,
                 timeGridPlugin,
                 interactionPlugin
+            ],
+            // カレンダーに表示するスケジュール一覧
+            calendarEvents:  [
+                {
+                    title: '報告会',
+                    start: '2020-02-20T10:00:00',
+                    end : '2020-03-09T12:30:00',
+                },
+                {
+                    title: 'ミーティング',
+                    start: '2020-02-12T10:30:00',
+                    end : '2020-02-12T12:30:00',
+                },
+                {
+                    title: '打ち合わせ',
+                    start: '2020-02-18T13:30:00',
+                    end : '2020-02-18T14:30:00',
+                },
             ]
+        }
+    },
+    methods: {
+        handleDateClick (arg) {
+            if (confirm('新しいスケジュールを' + arg.dateStr + 'に追加しますか ?')) {
+                this.calendarEvents.push({ // add new event data
+                    title: '新規スケジュール',
+                    start: arg.date,
+                    allDay: arg.allDay
+                })
+            }
         }
     }
 }
